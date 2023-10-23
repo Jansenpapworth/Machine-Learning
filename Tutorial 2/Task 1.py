7e63e06ff65a00effc9a53ed7a0489327b1b580e
jansenpapworth <jansenpapworth@live.com>
2023-10-23 10:50:28 +0100

    Tutorial 2

---
+++ Tutorial 2/Task 1.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from gaussian import gaussian
+
+x = np.linspace(-10,20,200)
+
+# calculating the distributions
+p_1 = gaussian(x,2,1.5)
+p_2 = gaussian(x,7,0.5)
+p_3 = gaussian(x,8,2.5)
+p_4 = gaussian(x,3.5,1)
+
+p_x_w1 = p_1 + p_2
+p_x_w2 = p_3 + p_4
+
+p_x_w1 = p_x_w1/np.trapz(p_x_w1,x)
+p_x_w2 = p_x_w2/np.trapz(p_x_w2,x)
+
+fig, ax = plt.subplots()
+plt.title('Likelihood')
+plt.plot(x,p_x_w1)
+plt.plot(x,p_x_w2)
+
+# prior probabilities
+p_prior1 = 0.5
+P_prior2 = 0.5
+
+
+# calculating the evidence terms
+p_x1 = (p_prior1*p_x_w1) + (p_prior1*p_x_w2)
+p_x2 = (P_prior2*p_x_w1) + (P_prior2*p_x_w2)
+
+P_pos1 = (p_x_w1*0.5)/p_x1
+P_pos2 = (p_x_w2*0.5)/p_x2
+
+fig, ax = plt.subplots()
+plt.title('Posterior')
+plt.xlim([-3, 15])
+plt.plot(x,P_pos1)
+plt.plot(x,P_pos2)
+plt.show()
+
--- Tutorial 2/gaussian.py
+++ Tutorial 2/gaussian.py
@@ -1,8 +1,7 @@
 ### function to calculate the gaussian function
 import numpy as np
+import matplotlib.pyplot as plt
 
 def gaussian(x,u,sig):
     g = (2*np.pi)**(-1/2)*1/sig*np.exp((-((x-u)/sig)**2)/2)
     return g
-
-print(gaussian(1,0,0.5))
